7c209df8efa550f63a9fb3ad88165b990630debf
unknown <take97531@lgcns.com>
2024-02-26 15:35:05 +0900

    최초 소스

---
+++ src/main/java/com/example/SuptPlcy/SuptPlcyApplication.java
@@ -0,0 +1,13 @@
+package com.example.SuptPlcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuptPlcyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuptPlcyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SuptPlcy/SuptPlcyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SuptPlcy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuptPlcyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
