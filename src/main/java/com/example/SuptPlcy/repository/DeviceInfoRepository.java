123b3a28a11c88374bca7b59506a483af7173554
unknown <A87749@cnspartner.com>
2024-02-29 14:18:00 +0900

    수정본 김수하

--- src/main/java/com/example/SuptPlcy/repository/DeviceInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SuptPlcy.repository;
-
-import com.example.SuptPlcy.entity.DeviceInfoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeviceInfoRepository extends JpaRepository<DeviceInfoEntity, String> {
-}
