04a960f4c956656ded711fb77e407107f5fe744c
KwonYuri <rnjsdbfl99@gmail.com>
2024-02-29 13:52:21 +0900

    고객정보 조회 api 수정 - 권유리

--- src/main/java/com/example/sales/controller/CustomerController.java
+++ src/main/java/com/example/sales/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.sales.controller;
 
+import com.example.sales.dto.CustomerDTO;
 import com.example.sales.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * @class      : 고객 정보 controller
+ * @작성자      : 이정호
+ * @최초작성일자 : 2024-02-07
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/sales")
@@ -17,8 +24,16 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    /**
+     * @name        : 고객정보 조회 by 전화번호
+     * @작성자       : 이정호
+     * @수정자       : 권유리
+     * @최초작성일자  : 2024-02-07
+     * @수정일자     : 2024-02-29
+     * @return
+     */
     @GetMapping("/checkCustomer")
-    public boolean checkCustomerByPhone(@RequestParam String phone) {
+    public CustomerDTO checkCustomerByPhone(@RequestParam String phone) {
         return customerService.checkCustomerByPhone(phone);
     }
 }
