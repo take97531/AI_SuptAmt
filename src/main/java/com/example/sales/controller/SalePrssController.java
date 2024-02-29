4dd551ba9dc814f7050124989c19df4a3811a956
wefasjie <jhs03272000@naver.com>
2024-02-29 14:29:36 +0900

    PK값 채번 및 시스템컬럼 세팅 추가

--- src/main/java/com/example/sales/controller/SalePrssController.java
+++ src/main/java/com/example/sales/controller/SalePrssController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,8 @@ public class SalePrssController {
     @Autowired
     private SalePrssService SalePrssService;
 
-    @PostMapping ("/saleprss")
-    public void salePrss(SalePrssDTO salePrssDTO) {
+    @PostMapping ("/presales")
+    public void salePrss(@RequestBody SalePrssDTO salePrssDTO) {
         SalePrssService.salePrss(salePrssDTO);
     }
 }
