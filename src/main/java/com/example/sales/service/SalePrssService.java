341f921c1f4c91596d8795555531b8f0d0119b24
wefasjie <jhs03272000@naver.com>
2024-03-03 11:08:35 +0900

    판매처리 return 추가

--- src/main/java/com/example/sales/service/SalePrssService.java
+++ src/main/java/com/example/sales/service/SalePrssService.java
@@ -4,9 +4,9 @@ import com.example.contract.entity.ContractInfoEntity;
 import com.example.contract.repository.ContractInfoRepository;
 import com.example.device.entity.DeviceInventoryEntity;
 import com.example.device.repository.DeviceInventoryRepository;
+import com.example.sales.dto.RsltDTO;
 import com.example.sales.dto.SalePrssDTO;
 import com.example.sales.entity.SaleInfoEntity;
-import com.example.sales.entity.SubscriptionInfoEntity;
 import com.example.sales.repository.SaleInfoRepository;
 import org.springframework.stereotype.Service;
 
@@ -28,44 +28,53 @@ public class SalePrssService {
         this.contractInfoRepository = contractInfoRepository;
     }
 
-    public void salePrss(SalePrssDTO salePrssDTO){
-
+    public RsltDTO salePrss (SalePrssDTO salePrssDTO) throws Exception {
+        RsltDTO rslt = new RsltDTO();
         LocalDateTime now = LocalDateTime.now();
-        // 단말재고정보 테이블 상태 update
-        Optional<DeviceInventoryEntity> optionalDeviceInventory = deviceInventoryRepository.findByDeviceCodeAndDeviceNumber(salePrssDTO.getDeviceCode(), salePrssDTO.getDeviceNumber());
-        if (optionalDeviceInventory.isPresent()) {
-            DeviceInventoryEntity deviceInventory = optionalDeviceInventory.get();
-            deviceInventory.setDeviceUsage("Y");
-            deviceInventoryRepository.save(deviceInventory);
+
+        try{
+            // 단말재고정보 테이블 상태 update
+            Optional<DeviceInventoryEntity> optionalDeviceInventory = deviceInventoryRepository.findByDeviceCodeAndDeviceNumber(salePrssDTO.getDeviceCode(), salePrssDTO.getDeviceNumber());
+            if (optionalDeviceInventory.isPresent()) {
+                DeviceInventoryEntity deviceInventory = optionalDeviceInventory.get();
+                deviceInventory.setDeviceUsage("Y");
+                deviceInventoryRepository.save(deviceInventory);
+            }
+
+            // 계약정보 update
+            Optional<ContractInfoEntity> optionalSubscriptionInfo = contractInfoRepository.findBySubscriptionId(salePrssDTO.getSubscriptionId());
+            if (optionalSubscriptionInfo.isPresent()) {
+                ContractInfoEntity subscriptionInfo = optionalSubscriptionInfo.get();
+                subscriptionInfo.setDeviceCode(salePrssDTO.getDeviceCode());
+                subscriptionInfo.setDeviceNumber(salePrssDTO.getDeviceNumber());
+                subscriptionInfo.setPlanCode(salePrssDTO.getPlanCode());
+                subscriptionInfo.setContractDatetime(now);
+                contractInfoRepository.save(subscriptionInfo);
+            }
+
+            // 판매정보 update
+            SaleInfoEntity saleInfo = new SaleInfoEntity();
+            saleInfo.setSaleId(UUID.randomUUID().toString());
+            saleInfo.setSubscriptionId(salePrssDTO.getSubscriptionId());
+            saleInfo.setSaleDatetime(now);
+            saleInfo.setDeviceCode(salePrssDTO.getDeviceCode());
+            saleInfo.setDeviceNumber(salePrssDTO.getDeviceNumber());
+            saleInfo.setDevicePrice(salePrssDTO.getDevicePrice());
+            saleInfo.setSupportAmount(salePrssDTO.getSupportAmount());
+            saleInfo.setSaleAmount(salePrssDTO.getSaleAmount());
+            saleInfo.setContractStartDatetime(now);
+            saleInfo.setContractEndDatetime(now.plusMonths(24));
+            saleInfo.setCreatedBy("정하성");
+            saleInfo.setCreateProgram("salePrss");
+            saleInfo.setCreateDatetime(now);
+
+            saleInfoRepository.save(saleInfo);
+
+            rslt.setRsltMsg("판매성공");
+            rslt.setRsltCd("Y");
+        } catch(Exception e){
+            throw new Exception("판매에 실패하였습니다.", e);
         }
-
-        // 계약정보 update
-        Optional<ContractInfoEntity> optionalSubscriptionInfo = contractInfoRepository.findBySubscriptionId(salePrssDTO.getSubscriptionId());
-        if (optionalSubscriptionInfo.isPresent()) {
-            ContractInfoEntity subscriptionInfo = optionalSubscriptionInfo.get();
-            subscriptionInfo.setDeviceCode(salePrssDTO.getDeviceCode());
-            subscriptionInfo.setDeviceNumber(salePrssDTO.getDeviceNumber());
-            subscriptionInfo.setPlanCode(salePrssDTO.getPlanCode());
-            subscriptionInfo.setContractDatetime(now);
-            contractInfoRepository.save(subscriptionInfo);
-        }
-
-        // 판매정보 update
-        SaleInfoEntity saleInfo = new SaleInfoEntity();
-        saleInfo.setSaleId(UUID.randomUUID().toString());
-        saleInfo.setSubscriptionId(salePrssDTO.getSubscriptionId());
-        saleInfo.setSaleDatetime(now);
-        saleInfo.setDeviceCode(salePrssDTO.getDeviceCode());
-        saleInfo.setDeviceNumber(salePrssDTO.getDeviceNumber());
-        saleInfo.setDevicePrice(salePrssDTO.getDevicePrice());
-        saleInfo.setSupportAmount(salePrssDTO.getSupportAmount());
-        saleInfo.setSaleAmount(salePrssDTO.getSaleAmount());
-        saleInfo.setContractStartDatetime(now);
-        saleInfo.setContractEndDatetime(now.plusMonths(24));
-        saleInfo.setCreatedBy("정하성");
-        saleInfo.setCreateProgram("salePrss");
-        saleInfo.setCreateDatetime(now);
-
-        saleInfoRepository.save(saleInfo);
+        return rslt;
     }
 }
